3c7bf10b2ffa0b4ab83534302702ac5a781ae40f
Ernad Husremovic <hernad@bring.out.ba>
2012-01-13 19:27:26 +0100

    bin => lnk

--- sprzi.sh
+++ sprzi.sh
@@ -2,7 +2,7 @@ export BASE_DIR=~/Desktop/knowhowERP_windows_serviser_3rd_party_install
 
 echo base dir = $BASE_DIR
 
-cp Git_knowhowERP.lnk $BASE_DIR
+cp Git_knowhowERP.lnk.bin $BASE_DIR/Git_knowhowERP.lnk
 
 cp -av  *.md *.bat util hbout "$BASE_DIR"
 
