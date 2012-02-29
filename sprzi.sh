4449405ff43df8352b2f546dd8669cf10b3c7d43
Ernad Husremovic <hernad@bring.out.ba>
2012-02-29 12:27:58 +0100

    sve lnk kopiraj

--- sprzi.sh
+++ sprzi.sh
@@ -11,9 +11,12 @@ else
     cp -av browse_dbf/browse_dbf.exe "$BASE_DIR"/util
 fi
 
-cp -av Git_knowhowERP.lnk "$BASE_DIR"/Git_knowhowERP.lnk
-cp -av "knowhowERP package updater.lnk" "$BASE_DIR"/
+#cp -av Git_knowhowERP.lnk "$BASE_DIR"/Git_knowhowERP.lnk
+cp -av *.lnk "$BASE_DIR"/
 
-cp -av  *.md *.bat util hbout "$BASE_DIR"
+cp -av  *.md "$BASE_DIR"
+cp -av  *.bat "$BASE_DIR"
+cp -av  util "$BASE_DIR"
+cp -av  hbout "$BASE_DIR"
 
 cp .bashrc "$BASE_DIR/.bashrc"
